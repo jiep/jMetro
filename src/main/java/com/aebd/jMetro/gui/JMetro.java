c8ee48b5793a8b0ba566739602b79670d1ba395b
José Ignacio Escribano <jiep@users.noreply.github.com>
2022-12-15 18:34:25 +0100

    Update JMetro.java

--- src/main/java/com/aebd/jMetro/gui/JMetro.java
+++ src/main/java/com/aebd/jMetro/gui/JMetro.java
@@ -53,7 +53,7 @@ public class JMetro extends JFrame {
 	 * Create the frame.
 	 */
 	public JMetro() {
-		super("JMetro v1.3.2");
+		super("JMetro v1.3.3");
 		this.setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 300);
